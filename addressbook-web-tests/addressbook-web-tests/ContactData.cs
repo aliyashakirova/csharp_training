7d036558112f19245aeaceda7916d942b168b43d
aliyashakirova <aliya.sulimova@gmail.com>
2021-08-01 16:45:07 +0300

    Creation of contact, creation of class containing properties of contacts.

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        private string company = "";
+        private string address = "";
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public ContactData(string firstname, string lastname, string company, string address)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.company = company;
+            this.address = address;
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public string Company
+        {
+            get
+            {
+                return company;
+            }
+            set
+            {
+                company = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+    }
+}
