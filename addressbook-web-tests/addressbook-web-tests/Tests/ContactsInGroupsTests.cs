22032bb30cc80fccd3940b5726a329f505bba70f
aliyashakirova <aliya.sulimova@gmail.com>
2021-10-21 17:10:48 +0300

    added several new checkings of contact addition/removal

--- addressbook-web-tests/addressbook-web-tests/Tests/ContactsInGroupsTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactsInGroupsTests.cs
@@ -12,6 +12,23 @@ namespace WebAddressbookTests
         [Test]
         public void TestAddingContactToGroup()
         {
+            
+            int numberOfGroups = GroupData.GetAll().Count();
+            int numberOfContacts = ContactData.GetAll().Count();
+            if (numberOfGroups == 0)
+            {
+                GroupData newGroup = new GroupData("name");
+                newGroup.Header = "header";
+                newGroup.Footer = "footer";
+                app.Groups.Create(newGroup);
+            }
+            if (numberOfContacts == 0)
+            {
+                ContactData newContact = new ContactData("fn", "ln");
+                newContact.Company = "ccc";
+                newContact.Address = "aaa";
+                app.Contacts.Create(newContact);
+            }
             GroupData group = GroupData.GetAll()[0];
       
             List<ContactData> oldList = group.GetContacts();
@@ -21,7 +38,20 @@ namespace WebAddressbookTests
             {
                 contactList.Remove(c);
             }
-            ContactData contact = contactList[0];
+            if (contactList.Count() == 0)
+            {
+                ContactData newContact = new ContactData("fn2", "ln2");
+                newContact.Company = "ccc2";
+                newContact.Address = "aaa2";
+                app.Contacts.Create(newContact);
+                contactList = ContactData.GetAll();
+                foreach (ContactData c in oldList)
+                {
+                    contactList.Remove(c);
+                }
+            }
+           
+                ContactData contact = contactList[0];
 
             app.Contacts.AddContactToGroup(contact, group);
 
@@ -34,9 +64,33 @@ namespace WebAddressbookTests
         [Test]
         public void TestRemoveContactFromGroup()
         {
+
+            int numberOfGroups = GroupData.GetAll().Count();
+            int numberOfContacts = ContactData.GetAll().Count();
+            if (numberOfGroups == 0)
+            {
+                GroupData newGroup = new GroupData("name");
+                newGroup.Header = "header";
+                newGroup.Footer = "footer";
+                app.Groups.Create(newGroup);
+            }
+            if (numberOfContacts == 0)
+            {
+                ContactData newContact = new ContactData("fn", "ln");
+                newContact.Company = "ccc";
+                newContact.Address = "aaa";
+                app.Contacts.Create(newContact);
+            }
+            ContactData contact = ContactData.GetAll()[0];
             GroupData group = GroupData.GetAll()[0];
+            
             List<ContactData> oldList = group.GetContacts();
-            ContactData contact = oldList[0];
+            int numberOfContactsInOldList = oldList.Count();
+            if (numberOfContactsInOldList == 0)
+            {
+                app.Contacts.AddContactToGroup(contact, group);
+            }
+            contact = group.GetContacts()[0];
 
             app.Contacts.RemoveContactFromGroup(contact, group);
 
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d07995d-9fe3-453c-940d-f8f4a7e242bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
